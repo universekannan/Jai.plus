191315569c000353edbf42075a0e36501e85b7ae
jasminesharmie <jasminesharmie@gmail.com>
2025-09-15 11:37:04 +0530

    jasminesharmie git commit -am jasminesharmie

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -24,7 +24,7 @@
                 @endphp
 
                 <div class="text-center mb-4">
-                    <h5 class="mt-3">T F C - Registration</h5>
+                    <h5 class="mt-3">JAI - Registration</h5>
                     <p class="mb-0">Please create your Crypto Wallet account</p>
                     @if($referrer)
                     <div class="col-12">
@@ -64,58 +64,16 @@
                     @endphp
 
                     <div class="input-group mb-3">
-                        <div class="input-group-prepend" style="max-width:150px;">
-                            <select class="form-control select2 @error('country_code') is-invalid @enderror"
-                                name="country_code" id="country_code" style="width:120px;">
-                                <option value="">Code</option>
-                                @foreach ($countries as $res)
-                                <option value="{{ $res->phone_code }}"
-                                    {{ old('country_code') == $res->phone_code ? 'selected' : '' }}>
-                                    {{ $res->iso3 }} ( +{{ $res->phone_code }} )
-                                </option>
-                                @endforeach
-                            </select>
-                        </div>
-
                         <input type="text" name="phone" value="{{ old('phone') }}"
                             class="form-control @error('phone') is-invalid @enderror" placeholder="Phone Number">
-
                         <div class="input-group-append">
                             <span class="input-group-text"><i class="fas fa-phone"></i></span>
                         </div>
-
                         @error('phone')
                         <span class="invalid-feedback">{{ $message }}</span>
                         @enderror
                     </div>
 
-
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend" style="max-width:150px;">
-                            <select class="form-control select2 @error('whatsapp_country_code') is-invalid @enderror"
-                                name="whatsapp_country_code" id="whatsapp_country_code" style="width:120px;">
-                                <option value="">Code</option>
-                                @foreach ($countries as $res)
-                                <option value="{{ $res->phone_code }}"
-                                    {{ old('whatsapp_country_code') == $res->phone_code ? 'selected' : '' }}>
-                                    {{ $res->iso3 }} ( +{{ $res->phone_code }} )
-                                </option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                        <input type="text" name="whatsapp_number" value="{{ old('whatsapp_number') }}"
-                            class="form-control @error('whatsapp_number') is-invalid @enderror"
-                            placeholder="WhatsApp Number">
-
-                        <div class="input-group-append">
-                            <span class="input-group-text"><i class="fab fa-whatsapp"></i></span>
-                        </div>
-
-                        @error('whatsapp_number')
-                        <span class="invalid-feedback">{{ $message }}</span>
-                        @enderror
-                    </div>
 
                     <div class="input-group mb-3">
                         <input type="password" name="password"
@@ -131,16 +89,6 @@
                         <div class="input-group-append"><span class="input-group-text"><i
                                     class="fas fa-lock"></i></span></div>
                     </div>
-
-                    <div class="input-group mb-3">
-                        <input type="text" name="wallet_address" value="{{ old('wallet_address') }}"
-                            class="form-control @error('wallet_address') is-invalid @enderror"
-                            placeholder="Crypto Wallet Address">
-                        <div class="input-group-append"><span class="input-group-text"><i
-                                    class="fas fa-wallet"></i></span></div>
-                        @error('wallet_address') <span class="invalid-feedback">{{ $message }}</span> @enderror
-                    </div>
-
                     <div class="row">
                         <div class="col-12">
                             <button type="submit" class="btn btn-primary btn-block">Register</button>
